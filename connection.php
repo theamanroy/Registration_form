40b6bac809314191b0c3af6b14f224839458e077
theamanroy <163314186+theamanroy@users.noreply.github.com>
2024-07-11 10:23:28 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="responsiveform3";
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+if($conn){
+    //echo"connection ok";
+}
+else{
+    echo"connection failed";
+}
+
+?>
